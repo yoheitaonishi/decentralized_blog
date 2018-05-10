cad14d480e0ce78ff1ed0daddd65894e70050faa
yoheitaonishi <onishiyoheita@gmail.com>
2018-05-10 09:48:20 +0100

    first commit

---
+++ contracts/MetaCoin.sol
@@ -0,0 +1,57 @@
+pragma solidity ^0.4.17;
+
+import "./ConvertLib.sol";
+
+// This is just a simple example of a coin-like contract.
+// It is not standards compatible and cannot be expected to talk to other
+// coin/token contracts. If you want to create a standards-compliant
+// token, see: https://github.com/ConsenSys/Tokens. Cheers!
+
+contract MetaCoin {
+	mapping (address => uint) balances;
+
+	event Transfer(address indexed _from, string _title, string _body);
+
+	function MetaCoin() public {
+		balances[tx.origin] = 10000;
+	}
+
+    function utfStringLength(string str) pure public
+    returns (uint length)
+    {
+        uint i=0;
+        bytes memory string_rep = bytes(str);
+
+        while (i<string_rep.length)
+        {
+            if (string_rep[i]>>7==0)
+                i+=1;
+            else if (string_rep[i]>>5==0x6)
+                i+=2;
+            else if (string_rep[i]>>4==0xE)
+                i+=3;
+            else if (string_rep[i]>>3==0x1E)
+                i+=4;
+            else
+                //For safety
+                i+=1;
+
+            length++;
+        }
+    }
+
+    function sendBlog(string title, string body) public returns(bool sufficient) {
+        require(0 < utfStringLength(title));
+        require(0 < utfStringLength(body));
+		Transfer(msg.sender, msg.sender, body);
+		return true;
+    }
+
+	function getBalanceInEth(address addr) public view returns(uint){
+		return ConvertLib.convert(getBalance(addr),2);
+	}
+
+	function getBalance(address addr) public view returns(uint) {
+		return balances[addr];
+	}
+}
