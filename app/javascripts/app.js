fec8ab3dfbc811a97e9469085bbc960fa71145e1
yoheitaonishi <onishiyoheita@gmail.com>
2018-05-28 02:18:46 +0000

    minifix

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -136,14 +136,11 @@ function addBlogAtTable(blogArray){
 }
 
 window.addEventListener('load', function() {
-  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
   if (typeof web3 !== 'undefined') {
     console.warn("Using web3 detected from external source. :) ")
-    // Use Mist/MetaMask's provider
     window.web3 = new Web3(web3.currentProvider);
   } else {
     console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development.");
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
     window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"));
   }
   App.start();
